3a1ba23a240d58fd4201979c1aa52ed5e66a9804
user126179663 <115039826+user126179663@users.noreply.github.com>
2023-04-18 17:08:21 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ addEventListener(
 																
 															},
 				guide = [
-					{ selectors: [ '#offer', '#create-offer-button' ], type: 'click', note: '手順を間違えたらお手数をかけしますがページを更新して初めからやり直してください。それではまず最初に"Create Offer"を書かれたボタンをクリックしてください。' },
-					{ selectors: [ '#offer', '#offer button[for="offer-input"]' ], type: 'click', note: 'クリックしてください。' },
+					{ selectors: [ '#offer', '#create-offer-button' ], type: 'click', note: '動作不良を起こしていると思われる時は恐れ入りますがページを更新して初めからやり直してください。またこのスクリプトではクリップボードへの書き込みを行ないます。クリップボードの中に保持されている情報がありましたら、お手数をおかけしますがあらかじめバックアップをお取りいただくようお願い申し上げます。それではまず最初に"Create Offer"と書かれたボタンを押してください。' },
+					{ selectors: [ '#offer', '#offer button[for="offer-input"]' ], type: 'click', note: '"Copy"と書かれたボタンを押してください。' },
 					{
 						selectors: [ '#answer', '#remote-offer-input' ],
 						type: 'focus',
@@ -25,8 +25,8 @@ addEventListener(
 						callback: pastingCallback,
 						option: { once: false }
 					},
-					{ selectors: [ '#answer', '#remote-offer-apply-button' ], type: 'click', note: 'クリックしてください。' },
-					{ selectors: [ '#answer', '#answer button[for="answer-input"]' ], type: 'click', note: 'クリックしてください。' },
+					{ selectors: [ '#answer', '#remote-offer-apply-button' ], type: 'click', note: '"Copy"と書かれたボタンを押してください。' },
+					{ selectors: [ '#answer', '#answer button[for="answer-input"]' ], type: 'click', note: '"Copy"と書かれたボタンを押してください。' },
 					{
 						selectors: [ '#offer', '#remote-answer-input' ],
 						type: 'focus',
@@ -34,7 +34,7 @@ addEventListener(
 						callback: pastingCallback,
 						option: { once: false }
 					},
-					{ selectors: [ '#answer', '#ice-servers-copy-button' ], type: 'click', note: 'クリックしてください。' },
+					{ selectors: [ '#answer', '#ice-servers-copy-button' ], type: 'click', note: '"Copy All"と書かれたボタンを押してください。' },
 					{
 						selectors: [ '#offer', '#remote-ice-input' ],
 						type: 'focus',
@@ -42,12 +42,14 @@ addEventListener(
 						callback: pastingCallback,
 						option: { once: false }
 					},
-					{ selectors: [ '#offer', '#remote-answer-apply-button' ], type: 'click', note: 'クリックしてください。' },
-					{ selectors: [ '#offer', '#ice-add-button' ], type: 'click', note: 'クリックしてください。' },
-					{ selectors: [ '#result' ], type: 'click', note: 'お疲れ様でした。"' + document.getElementById('result-offer-elapse-label').textContent + '"がどちらもN/A以外の文字で止まっていれば終了です。Copyボタンをクリックすると結果がクリップボードにコピーされますので、それをご報告くださると幸いです。ご協力ありがとうございました。😊' }
+					{ selectors: [ '#offer', '#remote-answer-apply-button' ], type: 'click', note: '”Apply”と書かれたボタンを押してください。' },
+					{ selectors: [ '#offer', '#ice-add-button' ], type: 'click', note: '"Add"と書かれたボタンを押してください。' },
+					{ selectors: [ '#result' ], type: 'click', note: 'お疲れ様でした。"' + document.getElementById('result-offer-elapse-label').textContent + '"がどちらも"N/A"以外の文字で止まっていれば終了です。止まるまで10秒以上かかる場合があります。"Attempt Time"の文字が止まると、ツイートへのリンクボタンが現われますので、ご協力くださる際はそれを押してツイートしてくだされば幸いです。ありがとうございました。😊' }
 				],
 				l = guide.length,
-				pointer = document.createElement('div'),
+				pointer = document.getElementById('pointer'),
+				pointerProgress = pointer.querySelector('#guide-progress'),
+				pointerContent = pointer.querySelector('#pointer-content'),
 				offer = document.getElementById('offer').peer,
 				answer = document.getElementById('answer').peer,
 				changedSignalState =	({ target }) => target.signalingState === 'stable' &&
@@ -60,8 +62,14 @@ addEventListener(
 						document.getElementById('result-' + (peer === offer ? 'offer' : 'answer') + '-status').textContent = peer.iceConnectionState,
 						v = (''+((Date.now() - peer.elapse) / 1000)).split('.')
 						document.getElementById('result-' + (peer === offer ? 'offer' : 'answer') + '-elapse').textContent = v[0] + '.' + (v?.[1] ?? '0').padStart(3, '0') + 's'
-					};
-		let i,k,v, i0,l0, g, rect,nodeRect, resized, ro,io, elapse, waiting, callback;
+					},
+				guideEnabler = document.getElementById('guide-handler-checkbox'),
+				changedGuideEnabler =
+					() => document.body.classList[guideEnabler.checked ? 'add' : 'remove']('display-guide');
+		let i,k,v, i0,l0, g, rect,nodeRect, resized, ro,io, elapse, waiting, callback, mo;
+		
+		guideEnabler.addEventListener('change', changedGuideEnabler),
+		changedGuideEnabler(),
 		
 		answer.addEventListener('signalingstatechange', changedSignalState),
 		offer.addEventListener('signalingstatechange', changedSignalState),
@@ -75,40 +83,48 @@ addEventListener(
 			
 			const tweet = document.getElementById('result-tweet');
 			
+			++pointerProgress.value,
+			
 			tweet.href =	'https://twitter.com/intent/tweet?' +
-								'text=' + document.getElementById('result-data').textContent.trim() + '&' +
+								'text=' +
+									encodeURI(
+										document.getElementById('result-data').textContent.trim().replace(']Offer', '] Offer') +
+										' via "' + navigator.userAgent + '"'
+									) +
+									'&' +
 								'via=' + 'code_zerodivide';
 			
 		}),
 		
-		pointer.classList.add('pointer'), document.body.appendChild(pointer),
-		
+		pointer.classList.add('pointer'),
+		i1=-1,
 		(io = new IntersectionObserver(
 			() => {
 				
 				const dr = document.body.getBoundingClientRect(), pr = pointer.getBoundingClientRect();
 				let k,v;
 				
-				dr.top > pr.top && pointer.style.setProperty('--correction-top', dr.top - pr.top + 'px'),
-				dr.left > pr.left && pointer.style.setProperty('--correction-left', dr.left - pr.left + 'px'),
+				dr.top > pr.top && pointer.style.setProperty('--correction-top', parseInt(dr.top - pr.top) + 'px'),
+				dr.left > pr.left && pointer.style.setProperty('--correction-left', parseInt(dr.left - pr.left) + 'px'),
 				(dr.left + dr.width) < (pr.left + pr.width) &&
-					pointer.style.setProperty('--correction-right', (pr.left + pr.width) - (dr.left + dr.width) + 'px'),
+					pointer.style.setProperty('--correction-right', parseInt((pr.left + pr.width) - (dr.left + dr.width)) + 'px'),
 				(dr.top + dr.height) < (pr.top + pr.height) &&
-					pointer.style.setProperty('--correction-bottom', (dr.top + dr.height) - (pr.top + pr.height) + 'px');
+					pointer.style.setProperty('--correction-bottom', parseInt((dr.top + dr.height) - (pr.top + pr.height)) + 'px');
 				
 			},
 			{ root: document, threshold: 1 })
 		).observe(pointer),
 		
-		i = -1;
+		i = -1, pointerProgress.max = l;
 		while (++i < l) {
 			
 			i0 = -1, l0 = (g = guide[i]).selectors.length, node = document;
 			while (++i0 < l0) node = (node.shadowRoot || node)?.querySelector(g.selectors[i0]);
 			
-			pointer.dataset.note = g.note,
+			pointerProgress.value = i,
+			pointerContent.textContent = g.note,
 			
-			resized = (a) => {
+			resized = () => {
 				
 				rect = node.getBoundingClientRect();
 				for (k in rect) typeof (v = rect[k]) === 'number' && (
@@ -135,6 +151,9 @@ addEventListener(
 			},
 			addEventListener('resize', resized),
 			(ro = new ResizeObserver(resized)).observe(node),
+			
+			(mo = new MutationObserver(() => document.body.classList.contains('display-guide') && resized())).
+				observe(document.body, { attributes: true, attributeFilter: [ 'class' ]}),
 			
 			await	new Promise(
 										rs =>	node.addEventListener(
